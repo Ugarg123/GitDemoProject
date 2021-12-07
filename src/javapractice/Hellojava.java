3a289094a34dc0030344835725a48cd9be7aee4c
unknown <>
2021-12-07 18:11:32 +0530

    ADDED: method to print name

--- src/javapractice/Hellojava.java
+++ src/javapractice/Hellojava.java
@@ -2,6 +2,10 @@ package javapractice;
 
 public class Hellojava {
 	public static void main(String[] args) {
-		System.out.println("Hello Java");
+		printName("Udit garg");
 	}
+        
+        public static void printName(String name){
+         System.out.println(name);
+        }
 }
